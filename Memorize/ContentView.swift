cd53962e6b05681636f853dafa2a713698c98c89
Jayant <jayant.ptl@gmail.com>
2024-09-15 00:44:21 +0530

    Pass card content from list

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    let empjis = ["👻", "🎃", "🕷️", "😈"]
+    
     var body: some View {
         HStack {
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
+            ForEach(empjis.indices, id: \.self) { index in
+                CardView(content: empjis[index])
+            }
         }
         .foregroundColor(.orange)
         .padding()
@@ -21,7 +22,8 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    @State var isFaceUp = false
+    let content: String
+    @State var isFaceUp = true
     
     var body: some View {
         ZStack {
@@ -29,7 +31,7 @@ struct CardView: View {
             if isFaceUp {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text("👻").font(.largeTitle)
+                Text(content).font(.largeTitle)
             } else {
                 base.fill()
             }
