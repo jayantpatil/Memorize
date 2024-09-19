383357465c8d89471189994fe1eb43b5b760d3bd
Jayant <jayant.ptl@gmail.com>
2024-09-19 23:35:27 +0530

    Lecture 5 Protocols, enum, Optional

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -16,6 +16,7 @@ struct EmojiMemoryGameView: View {
                 .font(.largeTitle)
             ScrollView {
                 cards
+                    .animation(.default, value: viewModel.cards)
             }
             Button("Shuffle") {
                 viewModel.shuffle()
@@ -26,10 +27,13 @@ struct EmojiMemoryGameView: View {
     
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-            ForEach(viewModel.cards.indices, id: \.self) { index in
-                CardView(viewModel.cards[index])
+            ForEach(viewModel.cards) { card in
+                CardView(card)
                     .aspectRatio(2/3, contentMode: .fit)
                     .padding(4)
+                    .onTapGesture {
+                        viewModel.choose(card)
+                    }
             }
         }
         .foregroundColor(.orange)
@@ -92,14 +96,14 @@ struct CardView: View {
             Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text(card.cardContent)
+                Text(card.content)
                     .font(.system(size: 200))
                     .minimumScaleFactor(0.01)
                     .aspectRatio(1, contentMode: .fit)
             }
             .opacity(card.isFaceUp ? 1 : 0)
             base.fill().opacity(card.isFaceUp ? 0 : 1)
-        }
+        }.opacity(card.isMatched ? 0 : 1)
     }
 }
 
--- Memorize/MemorizeGame.swift
+++ Memorize/MemorizeGame.swift
@@ -7,29 +7,56 @@
 
 import Foundation
 
-struct MemorizeGame<CardContent> {
+struct MemorizeGame<CardContent> where CardContent: Equatable {
    private(set) var cards: [Card]
     
     init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
         cards = []
         for pairIndex in 0..<max(2, numberOfPairsOfCards) {
             let cardContent = cardContentFactory(pairIndex)
-            cards.append(Card(cardContent: cardContent))
-            cards.append(Card(cardContent: cardContent))
+            cards.append(Card(content: cardContent, id: "\(pairIndex+1)a"))
+            cards.append(Card(content: cardContent, id: "\(pairIndex+1)b"))
         }
     }
     
-    func choose(_ card: Card) {
-        
+    var indexOfOneAndOnlyFaceUpCard: Int? {
+        get { cards.indices.filter { index in cards[index].isFaceUp }.only }
+        set { cards.indices.forEach { cards[$0].isFaceUp = ($0 == newValue) } }
+    }
+
+    mutating func choose(_ card: Card) {
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }) {
+            if !cards[chosenIndex].isFaceUp && !cards[chosenIndex].isMatched {
+                if let potentialMatchIndex = indexOfOneAndOnlyFaceUpCard {
+                    if cards[potentialMatchIndex].content == cards[chosenIndex].content {
+                        cards[potentialMatchIndex].isMatched = true
+                        cards[chosenIndex].isMatched = true
+                    }
+                } else {
+                    indexOfOneAndOnlyFaceUpCard = chosenIndex
+                }
+                cards[chosenIndex].isFaceUp = true
+            }
+        }
     }
     
     mutating func shuffle() {
         cards.shuffle()
     }
     
-    struct Card {
-        var isFaceUp: Bool = true
+    struct Card: Equatable, Identifiable, CustomDebugStringConvertible {
+        var isFaceUp: Bool = false
         var isMatched: Bool = false
-        let cardContent: CardContent
+        let content: CardContent
+        var id: String
+        var debugDescription: String {
+            "\(id): \(content) \(isFaceUp ? "up" : "down") \(isMatched ? " matched" : "")"
+        }
+    }
+}
+
+extension Array {
+    var only: Element? {
+        count == 1 ? first : nil
     }
 }
